1da96a579ac328c149102271ae4666a68cbaabbd
Kurtes <kurtesallen@gmail.com>
2026-06-11 04:03:48 -0400

    trigger pipeline after IAM fixes

--- terraform/grc/backend.tf
+++ terraform/grc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "acme-terraform-state"
-    key            = "grc/terraform.tfstate"
+    bucket         = "acme-terraform-state-846470648858"
+    key            = "environments/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "acme-terraform-locks"
     encrypt        = true
--- terraform/grc/oidc-role.tf
+++ terraform/grc/oidc-role.tf
@@ -23,19 +23,34 @@ resource "aws_iam_role" "github_grc_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
-      }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringEquals = {
-          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          "token.actions.githubusercontent.com:sub" = "repo:kurtesallen/acme-intake-governed-v4:*"
+    Statement = [
+      # --- GitHub OIDC trust ---
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:kurtesallen/acme-intake-governed-v5:*"
+          }
+        }
+      },
+
+      # --- Static IAM user fallback trust (with TagSession) ---
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::846470648858:user/kurt"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
       }
-    }]
+    ]
   })
 }
 
